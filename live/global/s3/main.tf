b579f3cbb5a611250f26bbee52107f238a2aa8c3
Jonathon Simonton <jsimonton020@gmail.com>
2020-09-28 09:05:57 -0700

    live directory, modules to seperate repo

---
+++ live/global/s3/main.tf
@@ -0,0 +1,44 @@
+provider "aws" {
+  region = "us-west-1"
+}
+
+resource  "aws_s3_bucket" "terraform_state"{
+  bucket = "js020-terraform-state"
+
+  # Prevent accidental deletion of this S3 bucket
+  lifecycle {
+    prevent_destroy = true
+  }
+
+  # Enable versioning so we can see the full revision history of our state files
+  versioning {
+    enabled = true
+  }
+
+  # Enable server-side encryption by default
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
+
+resource "aws_dynamodb_table" "terraform_locks" {
+  hash_key = "LockID"
+  name = "js020-terraform-locks"
+  billing_mode = "PAY_PER_REQUEST"
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
+
+# Partial configuration. The other settings (e.g., bucket, region) will be
+# passed in from a file via 'terraform init -backend-config=/home/jsimonton/terraform/setup/backend.hcl'
+terraform {
+  backend "s3" {
+    key = "global/s3/terraform.tfstate"
+  }
+}
